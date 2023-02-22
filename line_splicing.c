8d2199548d38e2abf19c0c3c3f99742b15ef9087
namaganesh <117300604+namaganesh@users.noreply.github.com>
2023-02-22 12:45:42 +0530

    Add files via upload

---
+++ line_splicing.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main()
+{
+	printf("Hello iam the first line : \n");
+	//first line done\
+	printf("Hello its second line ");
+	printf("hello world its third line\n");
+return 0;
+}	
+
